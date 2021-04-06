f7433ec46f0e0594f28666c36df94eabfa720705
musahaydar <musah@umich.edu>
2021-04-05 23:40:47 -0400

    Added schedule builder form input

--- js/autoscheduler.js
+++ js/autoscheduler.js
@@ -12,44 +12,24 @@ var HomeScreen = function (_React$Component) {
     function HomeScreen(props) {
         _classCallCheck(this, HomeScreen);
 
-        var _this = _possibleConstructorReturn(this, (HomeScreen.__proto__ || Object.getPrototypeOf(HomeScreen)).call(this, props));
-
-        _this.state = {
-            scheduleExists: false
-        };
-        return _this;
+        return _possibleConstructorReturn(this, (HomeScreen.__proto__ || Object.getPrototypeOf(HomeScreen)).call(this, props));
     }
 
-    // Remove this module from the DOM 
-    // Load the schedule builder component
-
-
     _createClass(HomeScreen, [{
-        key: "createSchedule",
-        value: function createSchedule() {}
-    }, {
-        key: "resumeSchedule",
-        value: function resumeSchedule() {}
-    }, {
         key: "render",
         value: function render() {
             return React.createElement(
                 "div",
-                { id: "homescreen" },
-                React.createElement(
-                    "h1",
-                    null,
-                    "Auto-scheduler"
-                ),
+                { id: "homeScreen" },
                 React.createElement(
                     "button",
-                    { onClick: this.createSchedule },
+                    { onClick: this.props.createSchedule },
                     "Create Schedule"
                 ),
                 React.createElement("br", null),
-                this.state.scheduleExists && React.createElement(
+                this.props.exists && React.createElement(
                     "button",
-                    { onClick: this.resumeSchedule },
+                    { onClick: this.props.resumeHandler },
                     "Resume Schedule"
                 )
             );
@@ -62,13 +42,223 @@ var HomeScreen = function (_React$Component) {
 var ScheduleBuilder = function (_React$Component2) {
     _inherits(ScheduleBuilder, _React$Component2);
 
-    function ScheduleBuilder() {
+    function ScheduleBuilder(props) {
         _classCallCheck(this, ScheduleBuilder);
 
-        return _possibleConstructorReturn(this, (ScheduleBuilder.__proto__ || Object.getPrototypeOf(ScheduleBuilder)).apply(this, arguments));
+        var _this2 = _possibleConstructorReturn(this, (ScheduleBuilder.__proto__ || Object.getPrototypeOf(ScheduleBuilder)).call(this, props));
+
+        _this2.state = {
+            numTasks: 1
+        };
+
+        _this2.addTask = _this2.addTask.bind(_this2);
+        return _this2;
     }
+
+    // Adds a new task input to the form
+
+
+    _createClass(ScheduleBuilder, [{
+        key: "addTask",
+        value: function addTask(event) {
+            event.preventDefault();
+            this.setState(function (state) {
+                return {
+                    numTasks: state.numTasks + 1
+                };
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            // Add rows to the table based on the number of tasks specified
+            var rows = [];
+            for (var i = 0; i < this.state.numTasks; ++i) {
+                rows.push(React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "td",
+                        null,
+                        React.createElement("input", { type: "text", id: "" })
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        React.createElement("input", { type: "number", id: "", min: "0", max: "100" }),
+                        React.createElement(
+                            "b",
+                            null,
+                            ":"
+                        ),
+                        React.createElement("input", { type: "number", id: "", min: "0", max: "100" })
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        React.createElement(
+                            "select",
+                            { id: "" },
+                            React.createElement(
+                                "option",
+                                { value: "1" },
+                                "Very Urgent"
+                            ),
+                            React.createElement(
+                                "option",
+                                { value: "2" },
+                                "Somewhat Urgent"
+                            ),
+                            React.createElement(
+                                "option",
+                                { value: "3" },
+                                "Not Urgent"
+                            )
+                        )
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        React.createElement("input", { type: "checkbox", id: "" })
+                    )
+                ));
+            };
+
+            return React.createElement(
+                "div",
+                { id: "scheduleBuilder" },
+                React.createElement(
+                    "h2",
+                    null,
+                    "Schedule Builder"
+                ),
+                React.createElement(
+                    "form",
+                    null,
+                    React.createElement(
+                        "table",
+                        null,
+                        React.createElement(
+                            "tr",
+                            null,
+                            React.createElement(
+                                "td",
+                                null,
+                                React.createElement(
+                                    "b",
+                                    null,
+                                    "Task Name"
+                                )
+                            ),
+                            React.createElement(
+                                "td",
+                                null,
+                                React.createElement(
+                                    "b",
+                                    null,
+                                    "Duration (hrs:mins)"
+                                )
+                            ),
+                            React.createElement(
+                                "td",
+                                null,
+                                React.createElement(
+                                    "b",
+                                    null,
+                                    "Urgency"
+                                )
+                            ),
+                            React.createElement(
+                                "td",
+                                null,
+                                React.createElement(
+                                    "b",
+                                    null,
+                                    "Add Breaks"
+                                )
+                            )
+                        ),
+                        rows.map(function (value, index) {
+                            return value;
+                        }),
+                        React.createElement(
+                            "tr",
+                            null,
+                            React.createElement("br", null)
+                        ),
+                        React.createElement(
+                            "tr",
+                            null,
+                            React.createElement("td", null),
+                            React.createElement("td", null),
+                            React.createElement("td", null),
+                            React.createElement(
+                                "td",
+                                null,
+                                React.createElement(
+                                    "button",
+                                    { onClick: this.addTask },
+                                    "Add Another Task"
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }]);
 
     return ScheduleBuilder;
 }(React.Component);
 
-ReactDOM.render(React.createElement(HomeScreen, null), document.getElementById('reactEntry'));
+// parent react component
+// contains the state of the scheduler and renders the components
+
+
+var AutoScheduler = function (_React$Component3) {
+    _inherits(AutoScheduler, _React$Component3);
+
+    function AutoScheduler(props) {
+        _classCallCheck(this, AutoScheduler);
+
+        var _this3 = _possibleConstructorReturn(this, (AutoScheduler.__proto__ || Object.getPrototypeOf(AutoScheduler)).call(this, props));
+
+        _this3.state = {
+            scheduleExists: false,
+            currentScreen: "HomeScreen"
+        };
+
+        _this3.createSchedule = _this3.createSchedule.bind(_this3);
+        return _this3;
+    }
+
+    // Remove this module from the DOM 
+    // Load the schedule builder component
+
+
+    _createClass(AutoScheduler, [{
+        key: "createSchedule",
+        value: function createSchedule() {
+            this.setState({
+                currentScreen: "ScheduleBuilder"
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { id: "container" },
+                this.state.currentScreen === "HomeScreen" && React.createElement(HomeScreen, {
+                    exists: this.state.scheduleExists,
+                    createSchedule: this.createSchedule
+                }),
+                this.state.currentScreen == "ScheduleBuilder" && React.createElement(ScheduleBuilder, null)
+            );
+        }
+    }]);
+
+    return AutoScheduler;
+}(React.Component);
+
+ReactDOM.render(React.createElement(AutoScheduler, null), document.getElementById('reactEntry'));
--- src/autoscheduler.js
+++ src/autoscheduler.js
@@ -1,38 +1,127 @@
 class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            scheduleExists: false
-        };
-    }
-
-    // Remove this module from the DOM 
-    // Load the schedule builder component
-    createSchedule() {
-        
-    }
-    
-    resumeSchedule() {
-        
     }
 
     render() {
         return (
-            <div id="homescreen">
-                <h1>Auto-scheduler</h1>
-                <button onClick={ this.createSchedule }>Create Schedule</button>
+            <div id="homeScreen">
+                <button onClick={this.props.createSchedule}>Create Schedule</button>
                 <br />
-                { this.state.scheduleExists && <button onClick={this.resumeSchedule}>Resume Schedule</button> }
+                { this.props.exists && <button onClick={this.props.resumeHandler}>Resume Schedule</button> }
             </div>
         );
     }
 }
 
 class ScheduleBuilder extends React.Component {
-    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numTasks: 1
+        };
+
+        this.addTask = this.addTask.bind(this);
+    }
+
+    // Adds a new task input to the form
+    addTask(event) {
+        event.preventDefault();
+        this.setState((state) => ({
+            numTasks: state.numTasks + 1
+        }))
+    }
+
+    render() {
+        // Add rows to the table based on the number of tasks specified
+        let rows = [];
+        for(let i = 0; i < this.state.numTasks; ++i) {
+            rows.push(
+                <tr>
+                    <td><input type="text" id=""></input></td>
+                    <td>
+                        <input type="number" id="" min="0" max="100"></input>
+                        <b>:</b>
+                        <input type="number" id="" min="0" max="100"></input>
+                    </td>
+                    <td>
+                        <select id="">
+                            <option value="1">Very Urgent</option>
+                            <option value="2">Somewhat Urgent</option>
+                            <option value="3">Not Urgent</option>
+                        </select>
+                    </td>
+                    <td><input type="checkbox" id=""></input></td> 
+                </tr>
+            );
+        };
+
+        return (
+            <div id="scheduleBuilder">
+                <h2>Schedule Builder</h2>
+                <form>
+                    <table>
+                        <tr>
+                            <td><b>Task Name</b></td>
+                            <td><b>Duration (hrs:mins)</b></td>
+                            <td><b>Urgency</b></td>
+                            <td><b>Add Breaks</b></td>
+                        </tr>
+                        { rows.map((value, index) => {
+                            return value;
+                        }) }
+                        <tr><br/></tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><button onClick={this.addTask}>Add Another Task</button></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        );
+    }
+}
+
+
+// parent react component
+// contains the state of the scheduler and renders the components
+class AutoScheduler extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scheduleExists: false,
+            currentScreen: "HomeScreen",
+        };
+
+        this.createSchedule = this.createSchedule.bind(this);
+    }
+
+    // Remove this module from the DOM 
+    // Load the schedule builder component
+    createSchedule() {
+        this.setState({
+            currentScreen: "ScheduleBuilder"
+        });
+    }
+
+    render() {
+        return (
+            <div id="container">
+                { this.state.currentScreen === "HomeScreen" && <HomeScreen 
+                    exists={this.state.scheduleExists}
+                    createSchedule={this.createSchedule}
+                /> }
+
+                { this.state.currentScreen == "ScheduleBuilder" && <ScheduleBuilder /> }
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
-    <HomeScreen />,
+    <AutoScheduler />,
     document.getElementById('reactEntry')
 );
